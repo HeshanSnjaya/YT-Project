1b69499b6158d4cba12453eb02a1789135f502e4
HeshanSnjaya <heshansnjaya@gmail.com>
2024-02-11 23:54:07 +0530

    adding cross origin

--- src/main/java/com/example/YTAnalysis/controller/SubscribeController.java
+++ src/main/java/com/example/YTAnalysis/controller/SubscribeController.java
@@ -3,13 +3,11 @@ package com.example.YTAnalysis.controller;
 import com.example.YTAnalysis.service.SubscribeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/api/v1")
 public class SubscribeController {
 
